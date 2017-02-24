74d8b81f66e3605c5fc44c36243564bb5a173780
Etienne Dilocker <etienne@dilocker.de>
2017-02-24 13:59:04 +0100

    add solution for lesson 03 (add prop types check)

--- src/LikeButton.js
+++ src/LikeButton.js
@@ -1,12 +1,25 @@
-import React from 'react';
+import React, { PropTypes } from 'react';
+
+const propTypes = {
+  likes: PropTypes.number,
+  onClick: PropTypes.func,
+}
+
+const defaultProps = {
+  onClick: () => alert('I am the default click handler')
+}
 
 const LikeButton = ({ likes, onClick }) => (
   <button
     onClick={onClick}
     style={{ backgroundColor: likes ? '#4169E1' : '#D3D3D3' }}
   >
-    I have { likes } likes, but you can click me!
+    I have {likes} likes, but you can click me!
   </button>
 );
 
+LikeButton.propTypes = propTypes;
+LikeButton.defaultProps = defaultProps;
+
 export default LikeButton;
+
