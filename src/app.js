a615205c6631ee04da12d70530a1bded68749db8
Etienne Dilocker <etienne@dilocker.de>
2017-02-28 18:15:30 +0100

    add redux without reducers

--- src/app.js
+++ src/app.js
@@ -1,9 +1,18 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
+import { Provider } from 'react-redux'
+import { createStore } from 'redux'
 
 import TodoApp from './TodoApp';
+import reducers from './ducks'
+
+const store = createStore(reducers)
+
+console.log(store.getState());
 
 ReactDOM.render(
-  <TodoApp />,
+  <Provider store={store}>
+    <TodoApp />
+  </Provider>,
   document.getElementById('app-root')
 );
