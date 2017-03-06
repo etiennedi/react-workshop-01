24e47b220ac2b8b32c27a7830518a0758b956d58
Etienne Dilocker <etienne@dilocker.de>
2017-03-06 16:45:54 +0100

    add redux devtools

--- src/app.js
+++ src/app.js
@@ -2,11 +2,12 @@ import React from 'react';
 import ReactDOM from 'react-dom';
 import { Provider } from 'react-redux'
 import { createStore } from 'redux'
+import { composeWithDevTools } from 'redux-devtools-extension';
 
 import TodoApp from './TodoApp';
 import reducers from './ducks'
 
-const store = createStore(reducers)
+const store = createStore(reducers, composeWithDevTools());
 
 console.log(store.getState());
 
