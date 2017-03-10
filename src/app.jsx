849b6240d03d9330c5be33bf981f4a1d6ad45567
Achim Rolle <info@achimrolle.de>
2017-03-10 16:10:47 +0100

    add solution for lesson-10

--- src/app.jsx
+++ src/app.jsx
@@ -5,17 +5,69 @@ import ReactDOM from 'react-dom';
 import { Provider } from 'react-redux';
 import { createStore } from 'redux';
 import { composeWithDevTools } from 'redux-devtools-extension';
+import {
+  BrowserRouter as Router,
+  Route,
+  NavLink,
+} from 'react-router-dom';
 
 
 import reducers from './ducks';
-import Form from './ContactForm';
+import Contact from './ContactForm';
+import Home from './Home';
+import About from './About';
 import './styles.css';
 
 const store = createStore(reducers, composeWithDevTools());
 
 ReactDOM.render(
   <Provider store={store}>
-    <Form />
+    <Router>
+      <div>
+        <ul className="navigation">
+          <li>
+            <NavLink
+              exact
+              activeClassName="active"
+              to="/"
+            >
+              Home
+            </NavLink>
+          </li>
+          <li>
+            <NavLink
+              activeClassName="active"
+              to="/about"
+            >
+              About
+            </NavLink>
+          </li>
+          <li>
+            <NavLink
+              activeClassName="active"
+              to="/contact"
+            >
+              Contact
+            </NavLink>
+          </li>
+        </ul>
+        <div className="content">
+          <Route
+            exact
+            path="/"
+            component={Home}
+          />
+          <Route
+            path="/about"
+            component={About}
+          />
+          <Route
+            path="/contact"
+            component={Contact}
+          />
+        </div>
+      </div>
+    </Router>
   </Provider>,
   document.getElementById('app-root'),
 );
--- webpack.config.js
+++ webpack.config.js
@@ -13,6 +13,7 @@ module.exports = {
     contentBase: path.join(__dirname, 'dist'),
     compress: true,
     port: 9000,
+    historyApiFallback: true,
   },
   resolve: {
     extensions: ['.js', '.jsx'],
