d8e2d84b242f2a161e60f03c09a66b7d8a737a0b
etiennedilocker <etienne@dilocker.de>
2017-03-13 20:48:56 +0100

    add solutions

--- src/Products.jsx
+++ src/Products.jsx
@@ -1,7 +1,28 @@
-import React from 'react';
+import React, { PropTypes } from 'react';
+import { connect } from 'react-redux';
+import { bindActionCreators } from 'redux';
+import { addProduct as addProductActionCreator } from './ducks/products';
 
-const Products = () => (
-  <div>Please connect me</div>
+const propTypes = {
+  products: PropTypes.array.isRequired,
+  addProduct: PropTypes.func.isRequired,
+};
+
+const Products = ({ products, addProduct }) => (
+  <div>
+    { JSON.stringify(products, null, 2)}
+    <button onClick={() => addProduct('Foo')}>Add product</button>
+  </div>
 );
 
-export default Products;
+Products.propTypes = propTypes;
+
+const mapStateToProps = state => ({
+  products: state.products,
+});
+
+const mapDispatchToProps = dispatch => bindActionCreators({
+  addProduct: addProductActionCreator,
+}, dispatch);
+
+export default connect(mapStateToProps, mapDispatchToProps)(Products);
