8eca490027da330e6195f6f19c7c210e8adfad60
etiennedilocker <etienne@dilocker.de>
2017-03-13 20:39:28 +0100

    fixed wrong reducer in root duck

--- src/ducks/index.js
+++ src/ducks/index.js
@@ -1,6 +1,6 @@
 import { combineReducers } from 'redux';
-import { reducer as formReducer } from 'redux-form';
+import productsReducer from './products';
 
 export default combineReducers({
-  form: formReducer,
+  products: productsReducer,
 });
