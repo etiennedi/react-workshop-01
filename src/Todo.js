a4b1602e5dcce8aad078a860263a05f0516d45ec
Dilocker Etienne <etienne.dilocker@metrosystems.net>
2018-01-28 14:18:36 +0100

    fix proptypes, update packages

--- src/Todo.js
+++ src/Todo.js
@@ -1,4 +1,5 @@
-import React, { PropTypes } from 'react';
+import React from 'react';
+import PropTypes from 'prop-types'
 
 const propTypes = {
     id: PropTypes.oneOfType([PropTypes.number, PropTypes.string]),
